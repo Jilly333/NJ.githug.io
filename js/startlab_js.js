3d75b94fef0687924441b0da46d7bb4f66f1d2b0
Jilly333 <87909704+Jilly333@users.noreply.github.com>
2022-01-16 16:06:21 +0800

    Delete js directory

--- js/startlab_js.js
+++
@@ -1,9 +0,0 @@
-function unprocess()
-{
-    alert("待开发！");
-}
-function lab_report()
-{
-    document.getElementById("report").style.display="block";
-    document.getElementById("home").style.opacity='0.3';
-}
